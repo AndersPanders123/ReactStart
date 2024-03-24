c2db8db763caf3162cf637e330e8421c289a6e82
AndersPanders123 <123541105+AndersPanders123@users.noreply.github.com>
2024-03-24 16:00:34 +0100

    kf

--- src/components/coop.tsx
+++ src/components/coop.tsx
@@ -1,11 +1,16 @@
-function coop(){
-    let money = 200;
+function coop() {
+  let money: number = 200;
+  let items: string[] = ["Monster, Cookies, Boller, Battery"];
 
-    return;
-    <div>
-      <p>Du har</p>
-    </div>;
-  }
+  return;
+  <div>
+    <p>"Du har {money}kr"</p>
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  </div>;
 }
 
-export default coop()
+export default coop();
